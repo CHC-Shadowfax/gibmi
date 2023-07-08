2922a6d0e36f8448e0e26721b87317d7fd709248
Cesar Huallpa <c.huallpa@alumni.ipade.mx>
2023-07-08 12:56:01 -0600

    tienen un error al crear el usuario

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,4 +68,13 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: 'mail.gibmi.com.mx',
+    port: 26,
+    user_name: 'latamtrip@gibmi.com.mx',
+    password: ENV['EMAIL_PASSWORD'],
+  }
 end
