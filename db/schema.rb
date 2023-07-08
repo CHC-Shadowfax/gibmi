76f68b50dd853ecadf598602c09da3e73558f09b
Gerardo Paz <paz2121@hotmail.com>
2023-07-08 11:56:21 -0600

    gem file schema

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_07_033256) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "blogs", force: :cascade do |t|
+    t.text "post"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_blogs_on_user_id"
+  end
+
   create_table "lists", force: :cascade do |t|
     t.string "name"
     t.text "description"
