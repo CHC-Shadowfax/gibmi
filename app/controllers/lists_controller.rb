026afdb3e2eaf0a4ba6430122a1721344a9462f7
Gerardo Paz <paz2121@hotmail.com>
2023-07-08 12:56:45 -0600

    Env

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,13 +1,51 @@
 class ListsController < ApplicationController
-  def new
+  before_action :set_list, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @lists = List.all
   end
 
   def show
   end
 
-  def index
+  def new
+    @list = List.new
+  end
+
+  def create
+    list = List.new(list_params)
+
+    if list.save
+      redirect_to lists_path
+    else
+      render :new
+    end
   end
 
   def edit
+  end
+
+  def update
+    if list.update(list_params)
+      redirect_to lists_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    list.destroy
+
+    redirect_to lists_path
+  end
+
+ private
+
+  def list_params
+    params.require(:list).permit(:name, :description, :event_date)
+  end
+
+  def set_list
+    @list = List.find(params[:id])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_07_033256) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_08_183131) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,6 +28,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_07_033256) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "event_date"
     t.index ["user_id"], name: "index_lists_on_user_id"
   end
 
