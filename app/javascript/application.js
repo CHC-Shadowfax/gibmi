1c6f5ee6ea98c489cedd7e2a57b671e10e146622
Anna Ibarra <annibrra@gmail.com>
2023-07-15 18:31:44 -0500

    algo de front

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,13 @@ import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
 
+import SlimSelect from 'slim-select'
+
+
+// on document ready
+
+document.addEventListener("turbo:load", function() {
+  new SlimSelect({
+    select: '#user_categories_list'
+  })
+});
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_14_031136) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_15_191428) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_14_031136) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_categories_on_user_id"
   end
 
   create_table "features", force: :cascade do |t|
@@ -124,6 +132,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_14_031136) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "categories", "users"
   add_foreign_key "features", "gifts"
   add_foreign_key "gift_links", "gifts"
   add_foreign_key "gifts", "lists"
