c038a4ef89e949b4227f1067a814845480adb13e
Anna Ibarra <annibrra@gmail.com>
2023-07-18 22:07:42 -0500

    favicon

--- app/models/gift.rb
+++ app/models/gift.rb
@@ -3,4 +3,9 @@ class Gift < ApplicationRecord
   belongs_to :list
 
   has_one_attached :photo
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :photo, presence: true
+  validates :address, presence: true
 end
