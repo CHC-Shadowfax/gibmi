f3d60a7324b60649d81f45006637a220676bb1ae
Lisette Gonzalez <lisetteclg.g@gmail.com>
2023-07-24 22:37:16 -0500

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,7 +26,7 @@ HOLIDAYS = [
     ID: 'CHRISTMAS',
     IMAGE: 'https://images.unsplash.com/photo-1545622783-b3e021430fee?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGNocmlzdG1hc3xlbnwwfHwwfHx8Mg%3D%3D&auto=format&fit=crop&w=800&q=60',
     DATE: "12-25-2023",
-    TITLE: "It's the most wonderful time of the year!",
+    TITLE: "It's Chirstmas Time!",
     FEATURED: true,
     CONTENT: "Christmas is a time for family and friends. It is a special time to be thankful for all of the wonderful blessings in our lives. Sending you good wishes and the hope that your holiday will be a joyous one."
   },
@@ -34,7 +34,7 @@ HOLIDAYS = [
     ID: 'JULY4TH',
     IMAGE: 'https://images.unsplash.com/photo-1436124026657-36828b43c7ce?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8NHRoJTIwb2YlMjBqdWx5fGVufDB8fDB8fHwy&auto=format&fit=crop&w=800&q=60',
     DATE: "07-04-2023",
-    TITLE: "Happy 4th of July",
+    TITLE: "Happy 4th of July!",
     FEATURED: false,
     CONTENT: "Happy 4th of July! I hope you have a fun and safe holiday."
   },
@@ -42,7 +42,7 @@ HOLIDAYS = [
     ID:'ANNASBIRTHDAY',
     IMAGE: 'https://res.cloudinary.com/dgeduaopr/image/upload/v1690248088/Lisset_-_2023-07-24_08_21_08_awodyy.jpg',
     DATE: "01-08-2023",
-    TITLE: "Happy Birthday Anna",
+    TITLE: "It's the BIG 30!",
     FEATURED: true,
     CONTENT: "Happy Birthday Anna! It's the BIG 30! Let's celebrate and make this a memorable year!"
   },
@@ -51,7 +51,7 @@ HOLIDAYS = [
     ID: 'FATHERSDAY',
     IMAGE:'https://images.unsplash.com/photo-1605812830455-2fadc55bc4ba?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGZhdGhlcnMlMjBkYXl8ZW58MHx8MHx8fDI%3D&auto=format&fit=crop&w=800&q=60',
     DATE: "06-18-2023",
-    TITLE: "Happy Father's Day",
+    TITLE: "Happy Father's Day!",
     FEATURED: false,
     CONTENT: "You are the best dad ever! I love you so much. Happy Father's Day!"
   },
@@ -60,7 +60,7 @@ HOLIDAYS = [
   ID: 'GRANDPARENTSDAY',
   IMAGE: 'https://images.unsplash.com/photo-1592381499682-33cee994fedd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fGdyYW5kJTIwcGFyZW50c3xlbnwwfHwwfHx8Mg%3D%3D&auto=format&fit=crop&w=800&q=60',
   DATE:"09-10-2023",
-  TITLE: "Happy Grandparents Day",
+  TITLE: "Happy Grandparents Day!",
   FEATURED: false,
   CONTENT: "Another day with your loving grandparents. Let's give thanks for having them another year and make them feel loved and celebrated."
   },
@@ -69,7 +69,7 @@ HOLIDAYS = [
     ID: 'VALENTINESDAY',
     IMAGE:'https://images.unsplash.com/photo-1484678002699-0e7dc5fa52c2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTAzfHx2YWxlbnRpbmVzJTIwZGF5fGVufDB8fDB8fHwy&auto=format&fit=crop&w=800&q=60',
     DATE:"02-14-2023",
-    TITLE: "Happy Valentine's Day",
+    TITLE: "Love is in the air!",
     FEATURED: true,
     CONTENT: "Today is the day to celebrate love! Happy Valentine's Day!"
   },
@@ -78,7 +78,7 @@ HOLIDAYS = [
   ID: 'MUSICIANSDAY',
   IMAGE: 'https://images.unsplash.com/photo-1525201548942-d8732f6617a0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Z3VpdGFyfGVufDB8fDB8fHwy&auto=format&fit=crop&w=800&q=60',
   DATE: "11-22-2023",
-  TITLE: "Happy Musicians Day",
+  TITLE: "Sound of Music",
   FEATURED: true,
   CONTENT: "Give the gift of music. Music is the universal language of mankind."
 },
@@ -96,7 +96,7 @@ HOLIDAYS = [
     ID: 'TEACHERSDAY',
     DATE: "05-15-2023",
     IMAGE: 'https://images.unsplash.com/photo-1511629091441-ee46146481b6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dGVhY2hlcnMlMjBkYXl8ZW58MHx8MHx8fDI%3D&auto=format&fit=crop&w=800&q=60',
-    TITLE: "Happy Teachers Day",
+    TITLE: "Teacher Appreciation Day",
     FEATURED: false,
     CONTENT: "Today is the day to celebrate teachers! They are the ones who teach us and guide us to be the best we can be. Happy Teachers Day!"
   },
@@ -106,7 +106,7 @@ HOLIDAYS = [
     ID: 'NATIONALWOMANSDAY',
     DATE: '08-09-2023',
     IMAGE: 'https://images.unsplash.com/photo-1498661694102-0a3793edbe74?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGdyb3VwJTIwb2YlMjB3b21hbnxlbnwwfHwwfHx8Mg%3D%3D&auto=format&fit=crop&w=800&q=60',
-    TITLE: "It's National Woman's Day",
+    TITLE: "National Woman's Day",
     FEATURED: false,
     CONTENT: "Woman have many talents and we can all think of that one person that has been a big example in our lives. Let's celebrate them today!"
   },
@@ -115,7 +115,7 @@ HOLIDAYS = [
     ID: 'NATIONALPETDAY',
     DATE: '04-11-2023',
     IMAGE: 'https://images.unsplash.com/photo-1560743641-3914f2c45636?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzd8fGRvZ3xlbnwwfHwwfHx8Mg%3D%3D&auto=format&fit=crop&w=800&q=60',
-    TITLE: "It's National Pet Day",
+    TITLE: "Celebrate our furry friends!",
     FEATURED: true,
     CONTENT: "Pets can be man's best friend. They are loyal and loving. Give a person the gift of having a loyal forever friend. Happy National Pet Day!"
   }
