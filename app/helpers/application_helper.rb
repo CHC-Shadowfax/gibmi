9eb3b2e743d86728c4f78246df70e5b9b76eecac
Lisette Gonzalez <lisetteclg.g@gmail.com>
2023-07-29 16:58:27 -0500

    updated

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
   def ensure_https(url)
     url.start_with?('http://', 'https://') ? url : "https://#{url}"
   end
+
+  def formatted_date(date)
+    date.strftime("%B %d of %Y")
+  end
 end
